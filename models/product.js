ede6a679f40e5a24dde31881013d851d3ab7c0fb
sebastianmb <sebastianmb88@gmail.com>
2023-06-22 16:17:00 -0500

    Creacion modelo de producto en el backend

---
+++ models/product.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const mongoose= require ('mongoose');
+const Schema= mongoose.Schema;
+
+
+
+const ProductSchema= Schema({
+    id: Number,
+    category:String,
+    nombre: String,
+    precio: Number,
+    cantidad: Number,
+    image: String
+
+});
+
+
+module.exports= mongoose.model('Product', ProductSchema);
+
+// Products ---> guarda documentos de este tipo y con la estructura dentro de la coleccion
