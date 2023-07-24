614818eb065a690765a9a17dd3373c4a0689c3aa
sebastianmb <sebastianmb88@gmail.com>
2023-07-23 22:35:58 -0500

    Creacion modelo compra

---
+++ models/compra.js
@@ -0,0 +1,18 @@
+const mongoose= require ('mongoose');
+const Schema= mongoose.Schema;
+
+
+
+const ProductSchema= Schema({
+    id: Number,
+    category:String,
+    nombre: String,
+    precio: Number,
+    cantidad: Number,
+    
+});
+
+
+module.exports= mongoose.model('Compra', ProductSchema);
+
+// Products ---> guarda documentos de este tipo y con la estructura dentro de la coleccion
